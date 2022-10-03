d97fdffb4d211292de39bc73970b931d27e1f694
Mohamed <ezzaroualymuhammad@gmail.com>
2022-10-03 18:37:29 +0100

    Build folder

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { register as registerServiceWorker } from './serviceWorkerRegistration.js';
 
 import App from './App';
 
@@ -14,3 +15,4 @@ root.render(
   </React.StrictMode>,
 );
 
+registerServiceWorker();
